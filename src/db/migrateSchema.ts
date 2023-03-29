6ee9060b8a885bb14cbe57502309fa92bf5bee50
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2023-03-29 22:39:42 +0800

    refactor migration script folder

--- src/db/migrateSchema.ts
+++ src/db/migrateSchema.ts
@@ -1,14 +1,15 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
-import postgres from 'postgres';
+import postgres from "postgres";
 
 const migrateSchema = async () => {
-  const migrationsClient = postgres("", {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const migrationsClient = postgres(process.env.PG_DB_URL!, {
     max: 1,
   });
   const db = drizzle(migrationsClient);
 
-  await migrate(db, { migrationsFolder: "./src/db/migrations" });
+  await migrate(db, { migrationsFolder: "./migrations" });
 };
 
 void migrateSchema()
